7b92c37d8fcb5a0ed77d2754acd53df00af2d79a
Jose Miguel Gallas <josemigallas@gmail.com>
2017-10-24 21:13:04 +0200

    implements Splash screen "the right way"

--- app/src/main/java/cz/devconf2017/Splash.java
+++ app/src/main/java/cz/devconf2017/Splash.java
@@ -4,34 +4,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-/**
- * Created by jridky on 10.12.16.
- */
-
 public class Splash extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.splash);
-
-        Thread timerThread = new Thread(){
-            public void run(){
-                try{
-                    super.run();
-                    sleep(1500);
-                }catch(Exception e){
-                    e.printStackTrace();
-                }finally {
-                    Intent i = new Intent(Splash.this, MainActivity.class);
-                    if(getIntent().getExtras() != null) {
-                        i.putExtras(getIntent().getExtras());
-                    }
-                    startActivity(i);
-                    finish();
-                }
-            }
-        };
-        timerThread.start();
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
     }
 }
